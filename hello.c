88e38c283783e764f52ca49e5a9c7545dbf57d4a
Khanh Le <kle15@husker.unl.edu>
2021-08-30 03:52:44 +0000

    added my major output

---
+++ hello.c
@@ -0,0 +1,15 @@
+/**
+ * Author: Khanh Le
+ * Date: 08/30/2021
+ *
+ */
+ #include <stdlib.h>
+ #include <stdio.h>
+
+ int main(int argc, char **argv) {
+
+     printf("Khanh Le\n");
+     printf("Computer Science.");
+
+     return 0;
+ }
